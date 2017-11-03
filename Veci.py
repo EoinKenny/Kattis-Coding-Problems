84833a3031c8d91a57e46c6fcab17ae0edea34b6
Eoin Kenny <eoinmkenny@gmail.com>
2017-11-03 20:37:49 +0000

    solution to Veci

--- Veci.py
+++ Veci.py
@@ -1,23 +1,45 @@
-"""
-Kattis Problem Veci
+import sys
 
-Your program will be given an integer X. Find the smallest number larger than X consisting of the same digits as X.
-"""
+    
+def permutations(word):
+    if len(word)<=1:
+        return [word]
 
-from itertools import combinations
-#### Example
-# 2416 => 2461
-# 330 => 0
+    #get all permutations of length N-1
+    perms=permutations(word[1:])
+    char=word[0]
+    result=[]
+    #iterate over all permutations of length N-1
+    for perm in perms:
+        #insert the character into every possible location
+        for i in range(len(perm)+1):
+            result.append(perm[:i].strip() + char.strip() + perm[i:].strip())
+    return result
 
-# The integer X (1 ≤ X ≤ 999999). The first digit in X will not be a zero.
-user_input = str(input())
-# Split into array of the numbers in String/Integer form
-str_numbers = list(user_input)
-int_numbers = [int(x) for x in str_numbers]
 
-# ============= The Stupid Way ================== #
-combinations = combinations(str_numbers)
+for user_input in sys.stdin:    
+    # Get all permutations
+    possbilities = permutations(user_input)
+    answers = list()
 
-for item in combinations:
-	print(item)
+    # Only keeep those which are bigger (first number only considered)
+    for answer in possbilities:
+        if int(answer[0]) >= int(user_input[0]):
+            answers.append(answer)
 
+    answers.sort()
+    found = False
+
+    # In case there's no answers (speed up)
+    if len(answers) == 0:
+        print(0)
+    else:
+        for answer in answers:
+            if answer > user_input:
+                print(answer)
+                found = True
+                break
+
+    if not found:
+        # If there's no answers
+        print(0)
